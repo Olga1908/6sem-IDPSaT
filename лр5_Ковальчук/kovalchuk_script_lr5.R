5ced411ea3c1956bd975d181454a2d5b250e3287
772302bsuir <63049048+772302bsuir@users.noreply.github.com>
2020-05-16 15:10:24 +0300

    add script for lab 5

---
+++ лр5_Ковальчук/kovalchuk_script_lr5.R
@@ -0,0 +1,24 @@
+olia <- seq(1,20)
+olia
+olia <- olia + 21
+olia
+olia <- olia * 3
+olia
+
+sum(olia)
+
+max(olia)
+
+min(olia)
+
+prod(olia)
+
+length(olia)
+
+mean(olia)
+
+var(olia)
+
+sort(olia)
+
+summary(olia)
